5a13755213c2a5d7c5ddfbab5fae1252d4acc37e
caozhanhao <cao2013zh@163.com>
2023-03-18 12:03:03 +0800

    chore: update copyright year.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
-#include <fstream>
-#include "../include/opqr/opqr.hpp"
+//   Copyright 2022 - 2023 opqr - caozhanhao
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+#include "opqr/opqr.hpp"
+
 using namespace opqr;
+
 int main()
 {
   std::vector<QR> examples;
@@ -9,7 +23,7 @@ int main()
    */
   //1 constructor
   QR qr1("https://github.com/caozhanhao/opqr");
-  QR qr2(std::vector{ 0,1,2,3 });//or other containers, e.g. std::list
+  QR qr2(std::vector{0, 1, 2, 3});//or other containers, e.g. std::list
   //2 add_data()
   QR qr3;
   QR qr4;
@@ -29,24 +43,24 @@ int main()
   qr6.set_level(ECLevel::L);
   qr6.set_mode(Mode::NUM);
   qr6.set_mask(6);
-
-
+  
+  
   /*
    *  Quiet Zone
    */
   qr6.disable_quiet_zone();
-
-
+  
+  
   /*
    *  Output
    */
   //1 PPM
-  qr1.generate().paint(pic::Format::PPM, "../../../examples/example1.ppm", 10);
+  qr1.generate().paint(pic::Format::PPM, "examples/example1.ppm", 10);
   //2 BMP
-  qr2.generate().paint(pic::Format::BMP, "../../../examples/example2.bmp", 10);
-  qr3.generate().paint(pic::Format::BMP, "../../../examples/example3.bmp", 10);
-  qr4.generate().paint(pic::Format::BMP, "../../../examples/example4.bmp", 10);
-  qr5.generate().paint(pic::Format::BMP, "../../../examples/example5.bmp", 10);
-  qr6.generate().paint(pic::Format::BMP, "../../../examples/example6.bmp", 10);
+  qr2.generate().paint(pic::Format::BMP, "examples/example2.bmp", 10);
+  qr3.generate().paint(pic::Format::BMP, "examples/example3.bmp", 10);
+  qr4.generate().paint(pic::Format::BMP, "examples/example4.bmp", 10);
+  qr5.generate().paint(pic::Format::BMP, "examples/example5.bmp", 10);
+  qr6.generate().paint(pic::Format::BMP, "examples/example6.bmp", 10);
   return 0;
 }
